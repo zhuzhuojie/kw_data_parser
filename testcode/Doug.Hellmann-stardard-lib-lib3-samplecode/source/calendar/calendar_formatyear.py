bc0e723f636dd735303ce6861c005b8db4f46b0a
zhuzhuojie <zhu.zhuojie@bluebank.com.cn>
2018-12-28 17:45:08 +0800

    add lib3

---
+++ testcode/Doug.Hellmann-stardard-lib-lib3-samplecode/source/calendar/calendar_formatyear.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Print entire year calendar.
+"""
+#end_pymotw_header
+import calendar
+
+cal = calendar.TextCalendar(calendar.SUNDAY)
+#print(cal.formatyear(2017, 2, 1, 1, 3))
+print(cal.formatyear(2019, 2, 1, 1, 3))
